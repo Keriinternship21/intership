844a843c4e0c71a100ee9fe6a56068b206ed53fe
NgocPhuong98 <80400280+Keriinternship21@users.noreply.github.com>
2021-03-29 09:47:47 +0700

    phuong update
    update

---
+++ infografik-agentur/administrator/logs/error.php
@@ -0,0 +1,13 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2021-03-19 06:46:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2021-03-19T06:46:28+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-03-19T06:46:38+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-03-19T06:46:49+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-03-19T06:46:53+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-03-24T05:23:11+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-03-25T01:35:29+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2021-03-28T01:17:09+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
